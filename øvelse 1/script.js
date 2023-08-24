1af88189ed5039d69cc1bf0145761160d74cb02c
Zara <Zara>
2023-08-24 10:26:07 +0200

    ændret text og h123

--- øvelse 1/script.js
+++ øvelse 1/script.js
@@ -1,14 +1,18 @@
 const h1 = document.querySelector("h1")
-console.log(h1)
+console.log(h1.textContent)
+h1.innerHTML = "overskrift";
 
 const h2 = document.querySelector("h2")
-console.log(h2)
+console.log(h2.textContent)
+h2.innerHTML = "h2 text";
 
 const h3 = document.querySelector("h3")
-console.log(h3)
+console.log(h3.textContent)
+h3.innerHTML = "h3 text";
 
 const p = document.querySelector("p")
-console.log(p)
+console.log(p.textContent)
+p.innerHTML = "hej med digggg"
 
 const img = document.querySelector("img")
 console.log(img)
