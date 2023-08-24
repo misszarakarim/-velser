bb0218aaa6a983d3ecb793faddd1fa07608770d9
Zara <Zara>
2023-08-24 10:17:04 +0200

    lavet 2a

---
+++ øvelse 1/script.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector("h1")
+console.log(h1)
+
+const h2 = document.querySelector("h2")
+console.log(h2)
+
+const h3 = document.querySelector("h3")
+console.log(h3)
+
+const p = document.querySelector("p")
+console.log(p)
+
+const img = document.querySelector("img")
+console.log(img)
+
