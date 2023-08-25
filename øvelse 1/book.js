bc81f6b6204d771f0f1aee2aa70d5456fc04b5d6
Zara <Zara>
2023-08-25 12:34:15 +0200

    add superhelt

---
+++ øvelse 1/book.js
@@ -0,0 +1,10 @@
+const firstBook = {
+    titel: "Ringenes Herre",
+    author: "Tkoien",
+    genre: ["Fantasy", "Drama", "Eventyr"],
+    pages: 751,
+    hasRead: true,
+    hasIllustrations: false
+}
+
+console.log(firstBook.author);
